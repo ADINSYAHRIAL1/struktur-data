9f51b4c6502dede318cc394af20956eeb06ad96c
ADINSYAHRIAL1 <146052113+ADINSYAHRIAL1@users.noreply.github.com>
2024-04-01 12:13:00 +0800

    senin/01/04/2004
    terimakasih

---
+++ STRUKTUR DATA/meet2.py
@@ -0,0 +1,26 @@
+# belajar Variabel
+# assigment
+x = 3
+y = 6
+uang = 1000
+
+#pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UAS saya =", y)
+print ("sisa uang saya =", uang)
+
+#penamaan variabel
+nilai_a = 9
+kotak10 = 10
+
+# input variabel kedua
+x = 50
+print ("Nilai UAS saya =", x)
+x = 100
+print ("Nilai UAS saya =", x)
+y = 19
+print ("Nilai UAS saya =", y)
+uang = 4000
+print ("sisa uang saya =", uang)
+
+
