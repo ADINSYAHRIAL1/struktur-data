59aa7d91845ecb4e0fd3fedb7a976fd4fbf1bc82
ADINSYAHRIAL1 <146052113+ADINSYAHRIAL1@users.noreply.github.com>
2024-04-24 20:01:47 +0700

    22/April/2024

---
+++ struktur data/exe3.py
@@ -0,0 +1,21 @@
+# contoh tipe data (integer) : angka satuan
+bil_integer = 1
+print(type(bil_integer))
+print("type data dari bilangan ini adalah:", type(bil_integer))
+
+# contoh tipe data (float) : bilangan desimal
+bil_float = 2,5
+print ("type data bil_float adalah: ", type (bil_float))
+
+# Contoh tipe data (string): kumpulan karakter
+bil_string = "adins"
+print("type data dari string_data adalah:", type(bil_string))
+
+# Contoh tipe data (boolean): biner 0,1, True, False
+bil_bol = True
+print("type data dari boolean_data adalah:", type(bil_bol))
+
+# tipe data dari bahasa c
+from ctypes import c_double
+bil_double = c_double (2000000)
+print("type data bil_double adalah:", type(bil_double))
