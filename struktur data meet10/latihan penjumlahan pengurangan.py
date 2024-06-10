5ce7d6cd8ba2ae077eeefeaef8cd8bc427a23bf1
ADINSYAHRIAL1 <146052113+ADINSYAHRIAL1@users.noreply.github.com>
2024-06-10 18:24:51 +0700

    senin 10/juni/2024

---
+++ struktur data meet10/latihan penjumlahan pengurangan.py
@@ -0,0 +1,12 @@
+# app penjumlahan penguragan
+# penjumlahan =1, pengurangan 2
+
+pilihan = input ("masukkan pilihan (1/2) :")
+
+if pilihan=="1":
+    print("anda masuk pejumlahan 1")
+elif pilihan=="2":
+    print("anda masuk pengurangan 2")
+else:
+    print("bukan penjumlahan 1 dan pengurangan 2")
+print("program selesai")
---
+++ struktur data meet10/meet10.py
@@ -0,0 +1,23 @@
+# belajar perbandingan lanjutan, ELIF
+
+nama = input ("masukkan nama :")
+
+if nama=="adins":
+    print("mahasiswa ganteng")
+elif nama=="jono":
+    print("mahasiswa kiyowo")
+elif nama=="nyek":
+    print("suara ayam")
+elif nama=="rizal":
+    print("mahasiswa tidur mk jam 8")
+elif nama=="sahrul":
+    print("mahasiswa suka kayang")
+elif nama=="ilham":
+    print("mahasiswa suka tobrut(tongkrongan bertobat brutal)")
+elif nama=="mas'ud":
+    print("mahasiswa bucin brutal")
+elif nama=="tuyul":
+    print("mahasiswa botak")
+else:
+    print("bukan mahasiswa")
+print("program selesai")
