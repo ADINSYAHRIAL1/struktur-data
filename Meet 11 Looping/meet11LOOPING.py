f5a598bfa28d4552543554131ed3b1ef2a6f8fb5
ADINSYAHRIAL1 <146052113+ADINSYAHRIAL1@users.noreply.github.com>
2024-06-28 23:33:07 +0700

    24 Juni 2024
    Lupa upload

---
+++ Meet 11 Looping/meet11LOOPING.py
@@ -0,0 +1,13 @@
+# Program untuk mencetak angka dari 1 sampai 10 menggunakan looping
+
+# Menggunakan loop for untuk mencetak angka dari 1 sampai 10
+print("Mencetak angka dari 1 sampai 10:")
+for i in range(1, 11):
+    print(i)
+
+# Menggunakan loop while untuk mencetak angka dari 1 sampai 10
+print("\nMencetak angka dari 1 sampai 10 menggunakan while loop:")
+j = 1
+while j <= 10:
+    print(j)
+    j += 1
